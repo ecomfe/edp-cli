7baac3a870ff9535fc36ad14ea82b076f8f39872
leeight <leeight@gmail.com>
2014-04-16 21:01:08 +0800

    bump to edp 1.0.0

--- tag.sh
+++ tag.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 VERSION="$1"
+BRANCH="$2"
+: ${BRANCH:="1.0.0-dev"}
 git commit -a -m "bump to $VERSION"
 git tag -a "$VERSION" -m "bump to $VERSION"
-git push origin 1.0.0-dev $VERSION
-npm publish
+git push origin $BRANCH $VERSION
+npm publish --registry=http://registry.npmjs.org
